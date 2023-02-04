483ca15cfc9cedc795b7f8d3ea3f956ec941ceb0
Daniil Lytin <lytin.daniil@gmail.com>
2023-02-04 20:22:48 +0800

    init spring boot project

---
+++ src/test/java/ru/z3r0ing/s3viewer/S3ViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.z3r0ing.s3viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
